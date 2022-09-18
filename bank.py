65e469f36eae50e2ae047bbb52cce674bb894ae2
rohmadarifin <rohmad.arifin@efishery.com>
2022-09-18 14:55:38 +0700

    Initial commit

---
+++ bank.py
@@ -0,0 +1,12 @@
+hai = input(" ").lower().replace(" ", "")
+y = hai[0:5]
+if(hai == "hello"):
+    money = 0
+elif(y == "hello"):
+    money = 0
+elif(hai[0] == "h"):
+    money = 20
+else:
+    money = 100
+
+print("$", money)
